81c06410ecc3c4fae39eff7fd2c172db7004a6b7
leo <leo.p@sourcegraph.com>
2023-02-27 09:12:07 +0000

    revisit

--- enterprise/internal/insights/background/retention_enqueuer.go
+++ enterprise/internal/insights/background/retention_enqueuer.go
@@ -8,6 +8,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/insights/store"
 	"github.com/sourcegraph/sourcegraph/internal/database/basestore"
 	"github.com/sourcegraph/sourcegraph/internal/goroutine"
+	"github.com/sourcegraph/sourcegraph/internal/timeutil"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
@@ -15,10 +16,10 @@ func newRetentionEnqueuer(ctx context.Context, workerBaseStore *basestore.Store,
 	return goroutine.NewPeriodicGoroutine(ctx,
 		"insights.retention.enqueuer",
 		"enqueues series retention jobs",
-		12*time.Hour,
+		2*time.Hour,
 		goroutine.HandlerFunc(
 			func(ctx context.Context) error {
-				seriesArgs := store.GetDataSeriesArgs{ExcludeJustInTime: true}
+				seriesArgs := store.GetDataSeriesArgs{ExcludeJustInTime: true, NextSnapshotBefore: timeutil.Now()}
 				allSeries, err := insightStore.GetDataSeries(ctx, seriesArgs)
 				if err != nil {
 					return errors.Wrap(err, "unable to fetch series for retention")
