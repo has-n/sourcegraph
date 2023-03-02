887f1b9ea97878c12b226a07aec70b1c53ea20e6
Eric Fritz <eric@eric-fritz.com>
2023-03-02 16:34:27 -0600

    WIP.

--- enterprise/internal/codeintel/sentinel/internal/background/cve_matcher.go
+++ enterprise/internal/codeintel/sentinel/internal/background/cve_matcher.go
@@ -9,13 +9,28 @@ import (
 )
 
 func NewCVEMatcher(store store.Store, metrics *Metrics, interval time.Duration) goroutine.BackgroundRoutine {
+	cveMatcher := &cveMatcher{
+		store: store,
+	}
+
 	return goroutine.NewPeriodicGoroutine(
 		context.Background(),
 		"codeintel.sentinel-cve-matcher", "TODO",
 		interval,
 		goroutine.HandlerFunc(func(ctx context.Context) error {
-			// Currently unimplemented
-			return nil
+			return cveMatcher.handle(ctx, metrics)
 		}),
 	)
+}
+
+type cveMatcher struct {
+	store store.Store
+}
+
+func (matcher *cveMatcher) handle(ctx context.Context, metrics *Metrics) error {
+	if err := matcher.store.ScanMatches(ctx); err != nil {
+		return err
+	}
+
+	return nil
 }
